3598fc6867bee686608bb2e9314568a42f72b04e
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-11-14 23:16:08 +0900

    Drill11 완성
    2018182041 홍순조
    커밋횟수: 5회
    https://youtu.be/isxwSqxkO8Y

--- Drill_11_2018182041/main_state.py
+++ Drill_11_2018182041/main_state.py
@@ -29,6 +29,16 @@ def collide(a, b):
     if bottom_a > top_b: return False
     return True
 
+def BricCollide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    if bottom_b-top_a<-5:return False
+    return True
 
 def enter():
     global boy
@@ -80,6 +90,7 @@ def update():
         boy.y+=1
     if collide(boy,brick):
         boy.y+=1
+        boy.x+=brick.fall_speed * game_framework.frame_time*brick.dir
 
     for ball in balls:
         if collide(boy,ball):
@@ -89,7 +100,7 @@ def update():
     for ball in balls:
         if collide(grass,ball):
             ball.stop()
-        if collide(ball,brick):
+        if BricCollide(brick,ball):
             ball.stop()
             ball.x+=brick.fall_speed * game_framework.frame_time*brick.dir
 
